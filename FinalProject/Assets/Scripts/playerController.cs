2292f0464053612293c0f7f49a13b2153e8a534f
adamhanniganGamesDeveloper <adamhannigan81@gmail.com>
2015-05-20 12:15:04 +1000

    New Prefabs, Powers & More

--- FinalProject/Assets/Scripts/playerController.cs
+++ FinalProject/Assets/Scripts/playerController.cs
@@ -1,25 +1,21 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class playerController : MonoBehaviour {
 
 	//SUPERPOWERS
 	private float runningSpeed = 1.0f;
-	private bool isFlash = false;
-	private bool isInvisible = false;
-	private bool isHulk = false;
-	private bool isSuperMan = false;
-	private bool isMutated = false;
+	private bool _flying = false;
+	private bool _isHulk = false;
 
 	private float isometricAngle =  Mathf.Tan ((26.5f * Mathf.PI)/180);
 
-	//float positionFromCenter = 0;
-	// Use this for initialization
+	//Sorting Order
 	public int sortingOrder = 6;
 	private SpriteRenderer sprite;
 
+	//Movement
 	private float fractionMovement = 1.0f;
-
 	private Rigidbody2D _myRigidBody;
 	private bool _jumping;
 	private float _distanceFromGround = 0.0f;
@@ -29,20 +25,28 @@ public class playerController : MonoBehaviour {
 	private bool _slidingRight = false;
 	private float _distanceFromOriginSlide = 0.0f;
 
+	//Shadow
 	private GameObject shadow;
 	private Vector3 shadowPosition;
 	private Vector3 shadowScale;
 
+	//Player Collider
 	private GameObject playerCollider;
 	private Vector3 playerColliderPosition;
+
+	//Mechanim
+	private Animator animator;
+
+
 	
 	void Start () {
 		Time.timeScale = 1.0F;
+
 		_myRigidBody = GetComponent<Rigidbody2D>();
 		shadow = GameObject.Find ("shadow");
 		playerCollider = GameObject.Find ("playerCollider");
-
 		sprite = GetComponent<SpriteRenderer>();
+		animator = GetComponent<Animator>();
 
 	}
 	
@@ -78,6 +82,7 @@ public class playerController : MonoBehaviour {
 			if (_distanceFromGround < 0){
 				GetComponent<Animator> ().SetBool ("jumping", false);
 				_jumping = false;
+				animator.SetBool("jumping", false);
 			}
 		}
 		shadow.transform.position = shadowPosition;
@@ -85,21 +90,16 @@ public class playerController : MonoBehaviour {
 		playerCollider.transform.position = playerColliderPosition;
 	}
 
-	void giveSuperpower()
-	{
-
-	}
-	
-	//KEEP PLAYER COLLIDER ON GROUND WHEN JUMPING
 	
 	// Update is called once per frame
 	void Update () {
+
 		currentPosition = transform.position;
-		//float move = Input.GetAxisRaw("Horizontal");
+
 		float move = 0;
 
 
-		if(Input.GetKeyDown("left")){
+		if(Input.GetKeyDown("left") && !_jumping){
 			//move = -5;
 			//slideLeft();
 			if(sortingOrder != 7 && !_slidingRight && !_slidingLeft){
@@ -109,7 +109,7 @@ public class playerController : MonoBehaviour {
 			}
 		}
 
-		if(Input.GetKeyDown("right")){
+		if(Input.GetKeyDown("right") && !_jumping){
 
 			if(sortingOrder != 0 && !_slidingLeft && !_slidingRight){
 				_slidingRight = true;
@@ -122,24 +122,24 @@ public class playerController : MonoBehaviour {
 		
 		if (Input.GetKeyDown ("space")) {
 			Debug.Log ("Space hit");
-			_jumping = true;
-			_jumpingUp = true;
-			GetComponent<Animator> ().SetBool ("jumping", true);
+			if(!_jumping){
+
+				_jumping = true;
+				_jumpingUp = true;
+				animator.SetBool("jumping", true);
+			}
 		}
+
 		if (_jumping) {
 			jump ();
 		}
 
-		if (isMutated)
-			giveSuperpower ();
 
-		//float xMovement = (0.1732050808F/2)*runningSpeed;   //isometrix values
-		//float yMovement = (0.1F/2)*runningSpeed; 
 		float xMovement = 1 * runningSpeed/10;
-		float yMovement = isometricAngle * runningSpeed/10;
+		float yMovement = isometricAngle * 1.003f * runningSpeed/10;
 		
 		if (_slidingLeft) {
-			if(_distanceFromOriginSlide < 4.5f){ //fraction to move in between lines
+			if(_distanceFromOriginSlide < 4.2f){ //fraction to move in between lines
 				move -= 0.5f;
 				_distanceFromOriginSlide += 0.5f; 
 			}
@@ -149,7 +149,7 @@ public class playerController : MonoBehaviour {
 			}
 		} 
 		else if (_slidingRight) {
-			if(_distanceFromOriginSlide < 4.5f){
+			if(_distanceFromOriginSlide < 4.2f){
 				move += 0.5f;
 				_distanceFromOriginSlide += 0.5f;
 			}
@@ -180,29 +180,53 @@ public class playerController : MonoBehaviour {
 	{
 		Debug.Log (other.gameObject.name);
 		//if powerUp
-		if (other.gameObject.name == "fastPowerUp") {
 
-			isFlash = true;
-			isMutated = true;
+		if (other.transform.parent.gameObject.name.StartsWith("power")) {
+			switch(other.gameObject.name)
+			{
+				case "fastPowerUp" :StartCoroutine(RunningFast ());
+									break;
+				case "hulkPowerUp" : StartCoroutine(HulkSmash()); 
+				 					 break;
+				case "flyingPowerUp" : StartCoroutine(Flying()); 
+									   break;
+				default: Debug.Log ("No powerup with this name"); break;
+			}
+			Object.Destroy (other.transform.parent.gameObject, 0.0F);
 
-			StartCoroutine(RunningFast ());
-
-
-			//runningSpeed = 2;
-			Object.Destroy (other.gameObject, 0.0F);
-			Object.Destroy (GameObject.Find ("fastCube 1"));
-			GetComponent<Animator> ().SetBool ("runningFast", true);
-		} else if (!(other.gameObject.name == "hole" && _jumping)){
-			Application.LoadLevel ("beforeTest");
+		} 
+		else if (!(other.gameObject.name == "hole" && (_jumping || _flying))){
+			//reload the level because there was a crash
+			//Nested If statement because I can
+			if(!_isHulk)
+				Application.LoadLevel ("beforeTest");
 		}
 
 		
 	}
 
 	public IEnumerator RunningFast(){
+		GetComponent<Animator> ().SetBool ("fast", true);	
 		runningSpeed = 2.0f;
-		yield return new WaitForSeconds(2f); // waits 2 seconds
+		yield return new WaitForSeconds(3f); // waits 2 seconds
 		runningSpeed = 1.0f;
+		GetComponent<Animator> ().SetBool ("fast", false);
+	}
+
+	public IEnumerator HulkSmash(){
+		_isHulk = true;
+		GetComponent<Animator> ().SetBool ("hulk", true);
+		yield return new WaitForSeconds(3f); // waits 2 seconds
+		_isHulk = false;
+		GetComponent<Animator> ().SetBool ("hulk", false);
+	}
+
+	public IEnumerator Flying(){
+		_flying = true;
+		GetComponent<Animator> ().SetBool ("flying", true);
+		yield return new WaitForSeconds(3f); // waits 2 seconds
+		_flying = false;
+		GetComponent<Animator> ().SetBool ("flying", false);
 	}
 
 	
